3dc687cdff9826bb3312c378c9177d1e3a2929a6
Kosuke Sawai <theykosuke_0101@yahoo.co.jp>
2017-01-04 07:24:55 +0000

    profile-update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get    'signup', to: 'users#new'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
+  patch  'login' , to: 'sessions#update'
   delete 'logout', to: 'sessions#destroy'
    
   resources :users
